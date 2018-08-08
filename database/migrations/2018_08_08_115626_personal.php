f5e741f14b59384f6196c0014838619a0072199d
Trikaka <trikaka12@gmail.com>
2018-08-08 15:05:48 +0300

    UpMigrateTablePersonal

---
+++ database/migrations/2018_08_08_115626_personal.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Personal extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('nameFather');
+            $table->string('position');
+            $table->string('first_day_on_work');
+            $table->string('salary');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
